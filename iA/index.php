170af15ac54843d079ff1900c2f0f3fa93210941
Luis Miguel <107388149+luismiguel28@users.noreply.github.com>
2023-06-28 17:04:19 -0600

    Add files via upload

---
+++ iA/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chatbot de Superhéroes</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="chat">
+    <div class="mensaje-bienvenida">
+      <p>Hola, soy un chatbot que adivina superhéroes. Para empezar, dime algunas características del superhéroe en el que estás pensando.</p>
+    </div>
+    <div id="chatbox"></div>
+    <div class="entrada">
+      <input type="text" id="mensaje" placeholder="Escribe tu respuesta aquí...">
+      <button id="enviar">Enviar</button>
+    </div>
+  </div>
+  <script src="script.js"></script>
+</body>
+</html>
